d0a1de1fbbc38dfe50c56ac5d41c7f42d603a523
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-06-22 10:30:41 -0600

    alphabetical sort added

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -8,6 +8,8 @@ const APIURL = "http://localhost:8001/transactions"
 function AccountContainer() {
   const [transactions, setTransactions] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [sortedCategory, setSortedCategory] = useState(false);
+  const [sortedDescription, setSortedDescription] = useState(false);
 
   useEffect(() => {
     fetch(APIURL)
@@ -36,13 +38,39 @@ function AccountContainer() {
   function filterTransactions() {
     const displayList = transactions.filter(transaction => 
       transaction.description.toLowerCase().includes(searchTerm.toLowerCase()));
+    
+    if (sortedCategory) {
+      displayList.sort((a,b) => (a.category > b.category) ? 1 : -1)
+    }
+    if (sortedDescription) {
+      displayList.sort((a,b) => (a.description > b.description) ? 1 : -1)
+    }
     return displayList;
   }
 
+  function toggleCategorySorted() {
+    if (sortedDescription) {
+      setSortedDescription(false);
+    }
+    setSortedCategory(sortedCategory => !sortedCategory);
+  }
+
+  function toggleDescriptionSorted() {
+    if (sortedCategory) {
+      setSortedCategory(false);
+    }
+    setSortedDescription(sortedDescription => !sortedDescription);
+  }
 
   return (
     <div>
       <Search updateSearchTerm={updateSearchTerm}/>
+      <button className="ui button" onClick={toggleCategorySorted}>
+        Toggle Category Sort
+      </button>
+      <button className="ui button" onClick={toggleDescriptionSorted}>
+        Toggle Description Sort
+      </button>
       <AddTransactionForm addNewTransaction={addNewTransaction}/>
       <TransactionsList transactions={filterTransactions()}/>
     </div>
